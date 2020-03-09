e37eec1cdc31ab18d7a5745870cdb87a62e3e460
Tan Yikai <tanyikai1994@gmail.com>
2020-03-09 22:38:41 +0800

    Updates

---
+++ precache-manifest.a7cb3db0ad298d0eed56d4513a1de677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50732f657b0309d7ded37734bf2abfc7",
+    "url": "/git/index.html"
+  },
+  {
+    "revision": "3a3b9065f1af6556632b",
+    "url": "/git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d982f458e2d42df83360",
+    "url": "/git/static/js/2.90fcad10.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/git/static/js/2.90fcad10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3b9065f1af6556632b",
+    "url": "/git/static/js/main.1efe24a4.chunk.js"
+  },
+  {
+    "revision": "3945ca78b1897d07f0c0",
+    "url": "/git/static/js/runtime-main.07f40393.js"
+  }
+]);
